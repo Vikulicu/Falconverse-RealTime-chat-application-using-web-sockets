1f41cd3fd1c54c0ef01674b3d3b51ff5ece3cdd0
Vikulicu <164415806+Vikulicu@users.noreply.github.com>
2026-01-28 23:08:27 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // This allows production builds to successfully complete 
+    // Warning: This allows production builds to successfully complete 
     // even if your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-  // If you also have TypeScript errors, add this:
   typescript: {
+    // This allows production builds to successfully complete 
+    // even if your project has TypeScript errors.
     ignoreBuildErrors: true,
   }
 };
 
 module.exports = nextConfig;
-
-module.exports = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-}
